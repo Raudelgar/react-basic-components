2fe0cec46ccf3b40b7840b1aefeb1cd1069717b3
Raudelgar <raudel24@gmail.com>
2020-08-15 21:08:48 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./styles.css";
+import LoginForm from "./components/login/LoginForm";
+
+export default function App() {
+  return (
+    <div className="App">
+      <LoginForm />
+    </div>
+  );
+}
